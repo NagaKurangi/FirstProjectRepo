17d9144940242e90d34a3a7d3436d09e580dafbe
NKurangi <buildcareerwithjava@gmail.com>
2022-10-18 12:49:05 +0530

    changes made in Demo.java added new class

--- Demo.java
+++ Demo.java
@@ -1,7 +1,16 @@
+class TestCode
+{
+	void m1()
+	{
+		System.out.println("TestCode");
+	}
+}
 class A
 {
 public static void main(String[] args)
 	{
+		TestCode tc = new TestCode();
+		tc.m1();
 		System.out.println("Hello World");
 	}
 }
